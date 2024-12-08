17a0b77638f5bb0cc707e9e2a84a98f276148fec
Bogdan Gusiev <agresso@gmail.com>
2024-12-08 20:07:44 +0100

    Improve scripts

--- .github/workflows/scripts/version.sh
+++ .github/workflows/scripts/version.sh
@@ -2,14 +2,17 @@
 
 set -e
 
-VERSION_FILE="lib/bogdan/test/gem/version.rb"
+VERSION_FILE="./lib/bogdan/test/gem/version.rb"
 if [ ! -f "$VERSION_FILE" ]; then
   echo "Version file not found!"
   exit 1
 fi
-VERSION=$(grep -Eo 'VERSION\s*=\s*["'\''][0-9]+\.[0-9]+\.[0-9]+["'\'']' "$VERSION_FILE" | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
+
+VERSION=$(ruby -r $VERSION_FILE -e "puts Bogdan::Test::Gem::VERSION")
+
 if [ -z "$VERSION" ]; then
   echo "Could not extract version from $VERSION_FILE"
   exit 1
 fi
+
 echo "$VERSION"
