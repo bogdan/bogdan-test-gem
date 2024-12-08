ad75cfaa1bc3ab5cbb66263aff6719f2d8bf4e7e
Bogdan Gusiev <agresso@gmail.com>
2024-12-08 16:03:26 +0100

    Improve release workflow

--- changelog.sh
+++ changelog.sh
@@ -9,4 +9,6 @@ if [ -z "$VERSION" ]; then
   echo "VERSION is not specified"
   exit 1
 fi
-grep -A 100 "^## \[$VERSION\]" CHANGELOG.md | grep -B 100 '^## ' | sed '1d;$d'
+FILE="./release_changelog.md"
+echo "## Changelog" > $FILE
+grep -A 100 "^## \[$VERSION\]" CHANGELOG.md | grep -B 100 '^## ' | sed '1d;$d' >> $FILE
--- version.sh
+++ version.sh
@@ -3,4 +3,9 @@ if [ ! -f "$VERSION_FILE" ]; then
   echo "Version file not found!"
   exit 1
 fi
-grep -Eo 'VERSION\s*=\s*["'\''][0-9]+\.[0-9]+\.[0-9]+["'\'']' "$VERSION_FILE" | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+'
+VERSION=$(grep -Eo 'VERSION\s*=\s*["'\''][0-9]+\.[0-9]+\.[0-9]+["'\'']' "$VERSION_FILE" | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
+if [ -z "$VERSION" ]; then
+  echo "Could not extract version from $VERSION_FILE"
+  exit 1
+fi
+echo "$VERSION"
